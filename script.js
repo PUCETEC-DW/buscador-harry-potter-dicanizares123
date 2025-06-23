94035b7b51e39508297de69342f67afb7e1d3678
David Cañizares <davidusa1en1@gmail.com>
2025-06-22 22:06:45 -0500

    Terminado

--- script.js
+++ script.js
@@ -1,23 +1,24 @@
 const url = "https://hp-api.onrender.com/api/characters";
-const input = document.getElementById("inputNombre ");
-const filtroCasa = document.getElementById("filtroCasa");
+const input = document.getElementById("inputNombre");
 const boton = document.getElementById("btnBuscar");
 const resultado = document.getElementById("resultado");
-const imgPorDefecto = "https://via.placeholder.com/100x150?text=Sin+imagen";
+const filtroCasa = document.getElementById("filtroCasa");
+const imagenPorDefecto = "https://via.placeholder.com/100x150?text=Sin+imagen";
 
-function mostrarResultados(personaje) {
-  let nombre = "";
-}
+boton.addEventListener("click", buscarPersonaje);
+input.addEventListener("keypress", function (e) {
+  if (e.key === "Enter") buscarPersonaje();
+});
 
 function buscarPersonaje() {
-  const nombreBuscado = input.value.tolowerCase().trim();
+  const nombreBuscado = input.value.toLowerCase().trim();
   const casaSeleccionada = filtroCasa.value;
 
   fetch(url)
-    .then(res.json())
+    .then((res) => res.json())
     .then((data) => {
       let filtrados = data.filter((p) =>
-        p.name.tolowerCase().includes(nombreBuscado)
+        p.name.toLowerCase().includes(nombreBuscado)
       );
 
       if (casaSeleccionada !== "") {
@@ -28,14 +29,30 @@ function buscarPersonaje() {
       mostrarResultados(filtrados);
     })
     .catch((error) => {
-      resultado.innerHTML = "<p>Error al obtener datos.</p> ";
+      resultado.innerHTML = "<p>Error al obtener los datos.</p>";
       console.error(error);
     });
 }
 
-boton.addEventListener("click", buscarPersonaje);
-input.addEventListener("keypress", function (e) {
-  if (e.key === "Enter") {
-    buscarPersonaje();
+//Esta parte nesecito consultar
+function mostrarResultados(personajes) {
+  resultado.innerHTML = "";
+
+  if (personajes.length === 0) {
+    resultado.innerHTML = "<p>Personaje no encontrado</p>";
+    return;
   }
-});
+
+  personajes.forEach((personaje) => {
+    const tarjeta = document.createElement("div");
+    tarjeta.classList.add("tarjeta");
+
+    tarjeta.innerHTML = `
+      <img src="${personaje.image || imagenPorDefecto}" alt="${personaje.name}">
+      <h3>${personaje.name}</h3>
+      <p>Casa: ${personaje.house || "Desconocida"}</p>
+    `;
+
+    resultado.appendChild(tarjeta);
+  });
+}
